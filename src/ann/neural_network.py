a77ab55b2460c5fcd9a14996cadee14e6b86f66b
ApurvKshirsagar <apurvkshirsagar2004@gmail.com>
2026-03-09 00:08:46 +0530

    Treid fixiing backprop again

--- src/ann/neural_network.py
+++ src/ann/neural_network.py
@@ -40,27 +40,37 @@ class NeuralNetwork:
         return a   # Raw logits
 
 
-    def backward(self,grad_logits,y=None):
-        grad_W_list = []
-        grad_b_list = []
+    # def backward(self,grad_logits,y=None):
+    #     grad_W_list = []
+    #     grad_b_list = []
 
-        # Backprop through layers in reverse; collect grads so that index 0 = last layer
+    #     # Backprop through layers in reverse; collect grads so that index 0 = last layer
+    #     grad = grad_logits
+    #     for layer in reversed(self.layers):
+    #         grad = layer.backward(grad)
+    #         grad_W_list.append(layer.grad_W)
+    #         grad_b_list.append(layer.grad_b)
+
+    #     # create explicit object arrays to avoid numpy trying to broadcast shapes
+    #     self.grad_W = np.empty(len(grad_W_list), dtype=object)
+    #     self.grad_b = np.empty(len(grad_b_list), dtype=object)
+    #     for i, (gw, gb) in enumerate(zip(grad_W_list, grad_b_list)):
+    #         self.grad_W[i] = gw
+    #         self.grad_b[i] = gb
+
+    #     # print("Shape of grad_Ws:", self.grad_W.shape, self.grad_W[1].shape)
+    #     # print("Shape of grad_bs:", self.grad_b.shape, self.grad_b[1].shape)
+    #     return self.grad_W, self.grad_b
+
+    def backward(self, grad_logits, y=None):
+        if isinstance(grad_logits, tuple):
+            grad_logits = grad_logits[0]
+
         grad = grad_logits
         for layer in reversed(self.layers):
             grad = layer.backward(grad)
-            grad_W_list.append(layer.grad_W)
-            grad_b_list.append(layer.grad_b)
-
-        # create explicit object arrays to avoid numpy trying to broadcast shapes
-        self.grad_W = np.empty(len(grad_W_list), dtype=object)
-        self.grad_b = np.empty(len(grad_b_list), dtype=object)
-        for i, (gw, gb) in enumerate(zip(grad_W_list, grad_b_list)):
-            self.grad_W[i] = gw
-            self.grad_b[i] = gb
-
-        # print("Shape of grad_Ws:", self.grad_W.shape, self.grad_W[1].shape)
-        # print("Shape of grad_bs:", self.grad_b.shape, self.grad_b[1].shape)
-        return self.grad_W, self.grad_b
+            
+        return grad
 
     def update_weights(self,optimizer):
         optimizer.update(self.layers)
