Tried fixing backprop and best model F1 score

src/ann/neural_layer.py
@@ -23,19 +23,36 @@ class DenseLayer:
         self.grad_b = None
         self.activation = get_activation(activation) 
 
+    # def forward(self, X):
+    #     self.X = X
+    #     Z =  np.dot(X, self.W) + self.b  #As Z = XW + b
+    #     return self.activation.forward(Z) #A = g(Z)
+
+    # def backward(self, dA):
+    #     #Chain rule through activation first
+    #     dZ = self.activation.backward(dA)
+
+    #     m = self.X.shape[0] 
+    #     self.grad_W = np.dot(self.X.T, dZ) / m
+    #     self.grad_b = np.sum(dZ, axis=0, keepdims=True) / m
+
+    #     # Pass gradient back to previous layer
+    #     dX = np.dot(dZ, self.W.T)
+    #     return dX
+
     def forward(self, X):
+        if X.ndim == 1:
+            X = X.reshape(1, -1)
         self.X = X
-        Z =  np.dot(X, self.W) + self.b  #As Z = XW + b
-        return self.activation.forward(Z) #A = g(Z)
+        Z = np.dot(X, self.W) + self.b
+        return self.activation.forward(Z)
 
     def backward(self, dA):
-        #Chain rule through activation first
+        if dA.ndim == 1:
+            dA = dA.reshape(1, -1)
         dZ = self.activation.backward(dA)
-
-        m = self.X.shape[0] 
+        m = self.X.shape[0]
         self.grad_W = np.dot(self.X.T, dZ) / m
         self.grad_b = np.sum(dZ, axis=0, keepdims=True) / m
-
-        # Pass gradient back to previous layer
         dX = np.dot(dZ, self.W.T)
         return dX

src/ann/objective_functions.py
@@ -13,13 +13,26 @@ class CrossEntropyLoss: #Categorical Cross-Entropy
         self.y_true = None
         self.probs = None
 
-    def forward(self,X, y_true):
-        #X is logits
+    # def forward(self,X, y_true):
+    #     #X is logits
+    #     self.probs = self.softmax.forward(X)
+    #     self.y_true = y_true
+    #     batch = y_true.shape[0]
+    #     correct = self.probs[np.arange(batch),y_true]
+    #     return -np.log(correct + 1e-9).mean() #1e-9 is added to prevent log(0) from blowing up to -infinity
+
+    def forward(self, X, y_true):
+        # ensure X is 2D
+        if X.ndim == 1:
+            X = X.reshape(1, -1)
+        # ensure y_true is 1D array
+        y_true = np.atleast_1d(np.array(y_true))
+        
         self.probs = self.softmax.forward(X)
         self.y_true = y_true
         batch = y_true.shape[0]
-        correct = self.probs[np.arange(batch),y_true]
-        return -np.log(correct + 1e-9).mean() #1e-9 is added to prevent log(0) from blowing up to -infinity
+        correct = self.probs[np.arange(batch), y_true]
+        return -np.log(correct + 1e-9).mean()
 
     def backward(self): 
         #The actual derivate of softmax is complex, but when we use crossentropy with softmax, most things cancels and remaining term is simple
@@ -36,13 +49,27 @@ class MSELoss:
         self.probs = None
         self.diff = None
 
+    # def forward(self, X, y_true):
+    #     self.probs = self.softmax.forward(X)
+    #     batch, n_classes = X.shape
+    #     # OneHot Matrix: 
+    #     one_hot = np.zeros((batch, n_classes))
+    #     one_hot[np.arange(batch), y_true] = 1.0
+    #     #Difference between prediction and target
+    #     self.diff = self.probs - one_hot
+    #     return (self.diff ** 2).mean()
+
     def forward(self, X, y_true):
+        # ensure X is 2D
+        if X.ndim == 1:
+            X = X.reshape(1, -1)
+        # ensure y_true is 1D array
+        y_true = np.atleast_1d(np.array(y_true))
+
         self.probs = self.softmax.forward(X)
-        batch, n_classes = X.shape
-        # OneHot Matrix: 
+        batch, n_classes = self.probs.shape
         one_hot = np.zeros((batch, n_classes))
         one_hot[np.arange(batch), y_true] = 1.0
-        #Difference between prediction and target
         self.diff = self.probs - one_hot
         return (self.diff ** 2).mean()
 